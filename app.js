9d0766c8b99496b080ff7dd30bad0e8fa92cad1c
Henri Pihkala <hpihkala@users.noreply.github.com>
2017-02-14 20:13:45 +0200

    error logging and sensible json error message

--- app.js
+++ app.js
@@ -10,10 +10,6 @@ var {getAbi, deployContracts} = require("./src/compileContracts")
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -91,9 +87,8 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  console.log(err)
+  res.status(err.status || 500).send({error: err})
 });
 
 module.exports = app;
