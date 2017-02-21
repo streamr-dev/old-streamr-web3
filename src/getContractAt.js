85f38759c6a8951496db5e8e78afb3a832fae2d1
Juuso Takalainen <juuso.takalainen@streamr.com>
2017-02-21 12:50:01 +0200

    ETH-14: fetch ABI from etherscan.io API
    also nice live test, woo

--- src/getContractAt.js
+++ src/getContractAt.js
@@ -1,8 +1,13 @@
 var _ = require("lodash")
+var rest = require("restling");
+var debug = require('debug')('getContractAt')
 
 var Web3 = require("web3")
 var web3 = new Web3()
 web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'))
+
+// https://etherscan.io/myapikey (testnet seems to work without)
+//const API_KEY = "EKGYIQ8GW3F96C1PQAP6UV3MW8M4R7AJAI"
 
 function getContractAt(address) {
     if (!address) { return Promise.reject("'at' query parameter required (contract address)") }
@@ -10,8 +15,15 @@ function getContractAt(address) {
     if (bytecode.length < 3) { return Promise.reject("no contract at " + address) }
 
     // TODO: Query https://etherscan.io/apis#contracts
-
-    return Promise.resolve({address})
+    var url = `https://testnet.etherscan.io/api?module=contract&action=getabi&address=${address}`//&apikey=${API_KEY}`
+    return rest.get(url).then((response) => {
+        debug(`Response from ${url}: ${response.data}`)
+        if (response.data.result == "") {
+            return {address}
+        } else {
+            return {address, abi: JSON.parse(response.data.result)}
+        }
+    })
 }
 
 module.exports = getContractAt
--- test/getContractTest.js
+++ test/getContractTest.js
@@ -8,4 +8,15 @@ describe("getContractAt", () => {
             assert.equal(result.address, address)
         })
     })
+
+    it("should return ABI for a known contract", () => {
+        // see https://testnet.etherscan.io/address/0x7d86a87178d28f805716828837d1677fb7af6ff7#code
+        var address = "0x7d86a87178d28f805716828837d1677fb7af6ff7"
+        return getContractAt(address).then(result => {
+            assert.deepEqual(result, {
+                address,
+                abi: [{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"bytes32"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_owner","type":"address"}],"name":"setOwner","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"nextTimestamp","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"giveMe","outputs":[{"name":"success","type":"bool"}],"payable":true,"type":"function"},{"constant":false,"inputs":[{"name":"who","type":"address"}],"name":"giveTo","outputs":[{"name":"success","type":"bool"}],"payable":true,"type":"function"},{"constant":true,"inputs":[],"name":"delay","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"who","type":"address"}],"name":"giveForce","outputs":[{"name":"success","type":"bool"}],"payable":true,"type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_giveAway","type":"uint256"}],"name":"setGiveAway","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"giveAway","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"inputs":[{"name":"_giveAway","type":"uint256"},{"name":"_delay","type":"uint256"}],"payable":true,"type":"constructor"},{"payable":true,"type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"giveAway","type":"uint256"}],"name":"OnGiveAwayChanged","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"who","type":"address"},{"indexed":true,"name":"giveAway","type":"uint256"}],"name":"OnPaid","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"}],"name":"OnOwnerChanged","type":"event"}]
+            })
+        })
+    })
 })
