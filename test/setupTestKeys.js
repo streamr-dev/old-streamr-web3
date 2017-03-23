cd411d68b9cbe4daba407444de0800dd86e2af97
Juuso Takalainen <juuso.takalainen@streamr.com>
2017-03-23 21:53:54 +0200

    added testnet keys for tests

--- test/setupTestKeys.js
+++ test/setupTestKeys.js
@@ -3,3 +3,7 @@ const web3 = require("../src/signed-web3")
 // testrpc default accounts
 web3.streamr.setKeyForAddress("0xb3428050ea2448ed2e4409be47e1a50ebac0b2d2", "0x6e340f41a1c6e03e6e0a4e9805d1cea342f6a299e7c931d6f3da6dd34cb6e17d")
 web3.streamr.setKeyForAddress("0xe5019d79c3fc34c811e68e68c9bd9966f22370ef", "0xcb378e5fb243a34f6745a0dbed4032a2b8292b467b03f771d14f747200e75967")
+
+// testnet accounts
+web3.streamr.setKeyForAddress("0x9e3d69305da51f34ee29bfb52721e3a824d59e69", "0xf27470af84d6c0a793d08a549e10baa99c29e9445cec400b0038966cf2730551")
+web3.streamr.setKeyForAddress("0xddc8963569bc7f237fee4eaf129d26d276fcf22e", "0xb127395714f6677c88e3cd7faf32de1f05f2c8bc1b8d63f7f8fae3346951d0cc")
